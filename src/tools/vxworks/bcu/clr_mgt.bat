6e55de7a8b10fc90376c5d540c5add36862ca792
ice.zhao <smilebit@163.com>
2019-03-07 17:21:05 +0800

    let cmd.exe to stop.

---
+++ src/tools/vxworks/bcu/clr_mgt.bat
@@ -0,0 +1,1 @@
+D:\DevelopmentTools\Python27\python ..\..\..\autoworld.py -T "cmd" -f "clr_mgt.txt" -P "D:\DevelopmentTools\windriver\wrenv.exe -p vxworks-6.8"
--- src/tools/vxworks/bcu/compile_mgt.bat
+++ src/tools/vxworks/bcu/compile_mgt.bat
@@ -1,1 +1,2 @@
 D:\DevelopmentTools\Python27\python ..\..\..\autoworld.py -T "cmd" -f "compile_mgt.txt" -P "D:\DevelopmentTools\windriver\wrenv.exe -p vxworks-6.8"
+set /p input="Input Enter key to exit."
